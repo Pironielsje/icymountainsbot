be1cd9a888b6f7ce4b6d99380236905ccc4946c7
GlowCat <81696606+Pironielsje@users.noreply.github.com>
2022-02-25 19:06:50 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -5,13 +5,13 @@ module.exports.run = async(client, msg, args) => {
 
     if(!msg.member.permissions.has("KICK_MEMBERS")) return msg.reply("You have to have the **KICK_MEMBERS** permission to do this!")
 
-    const target = msg.mentions.users.first()
+    const target = msg.mentions.members.first()
 
     if(!target || !args[0]) msg.reply(`Who do you want to warn? Please tag someone while running the command!`)
 
     if(!parseInt(args[0])) msg.reply(`Please give me an id! Not some letters`)
 
-    const targetId = target.id || args[0]
+    const targetId = target.user.id || msg.guild.members.cache.get(args[0]).user.id
 
     let reason = args[1].join(" ")
 
