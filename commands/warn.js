eafcf4d62640e96c5090c9eb8459eb25498269fa
GlowCat <81696606+Pironielsje@users.noreply.github.com>
2022-02-25 19:38:37 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -22,7 +22,7 @@ module.exports.run = async(client, msg, args) => {
 
     if(data) {
         data.update(
-            { warns: { $inc: 1 }}
+            { $inc: { warns: 1 }}
         )
 
         msg.reply(`Warned <@${targetId}> for ${reason}`)
