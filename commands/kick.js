5d60ecddc9b1613f8a1aa4a44a7f5f23a002236f
GlowCat <81696606+Pironielsje@users.noreply.github.com>
2022-02-26 20:06:44 +0100

    fixing the "message" instead of msg

--- commands/kick.js
+++ commands/kick.js
@@ -1,31 +1,31 @@
 module.exports.run = async(client, msg, args) => {
-    if(!message.member.permissions.has("KICK_MEMBERS")) return message.reply("Sorry! You have to have the: **KICK_MEMBERS** permission to use this")
-    if(!message.guild.me.permissions.has("KICK_MEMBERS")) return message.reply("Sorry! I don't have the permission to kick someone!")
+    if(!msg.member.permissions.has("KICK_MEMBERS")) return msg.reply("Sorry! You have to have the: **KICK_MEMBERS** permission to use this")
+    if(!msg.guild.me.permissions.has("KICK_MEMBERS")) return msg.reply("Sorry! I don't have the permission to kick someone!")
  
-    if(!args[0]) return message.reply("Please mention at least one person or give atleast one id to ban!")
-    if(!args[1]) return message.reply("Please give me a reason to ban this person!")
+    if(!args[0]) return msg.reply("Please mention at least one person or give atleast one id to ban!")
+    if(!args[1]) return msg.reply("Please give me a reason to ban this person!")
  
-    const user = message.guild.members.cache.get(message.mentions.users.first().id || message.guild.members.get(args[0])).id
+    const user = msg.guild.members.cache.get(msg.mentions.users.first().id || msg.guild.members.get(args[0])).id
  
-    if(!user) return message.reply("I can't seem to find this user. Is the id or mention correct?")
+    if(!user) return msg.reply("I can't seem to find this user. Is the id or mention correct?")
  
-    if(user.permissions.has("MANAGE_MESSAGES")) return message.reply("Couldn't kick a moderator.")
+    if(user.permissions.has("MANAGE_msgS")) return msg.reply("Couldn't kick a moderator.")
  
     var reason = args[1].slice(1).join(" ")
  
-    const banned = new MessageEmbed()
+    const banned = new msgEmbed()
         .setColor("RED")
-        .setDescription(`Kicked **${user} (${user.id})** For **${reason}** by **${message.author.username}**`)
+        .setDescription(`Kicked **${user} (${user.id})** For **${reason}** by **${msg.author.username}**`)
         .setFooter(`Requested by: ${msg.author.username}`, msg.author.displayAvatarURL())
         .setTimestamp()
  
     user.kick(reason).catch(err => {
         if(err) {
-            message.reply('Something went wrong')
+            msg.reply('Something went wrong')
             console.log(err)
         }
     })
-    message.reply({embeds: [banned]})
+    msg.reply({embeds: [banned]})
 }
 
 module.exports.help = {
--- commands/unban.js
+++ commands/unban.js
@@ -1,19 +1,19 @@
 module.exports.run = async(client, msg, args) => {
-    if(!message.member.permissions.has("BAN_MEMBERS")) return message.reply("Sorry! You have to have the: **BAN_MEMBERS** permission to use this")
-    if(!message.guild.me.permissions.has("BAN_MEMBERS")) return message.reply("Sorry! I don't have the permission to ban someone!")
+    if(!msg.member.permissions.has("BAN_MEMBERS")) return msg.reply("Sorry! You have to have the: **BAN_MEMBERS** permission to use this")
+    if(!msg.guild.me.permissions.has("BAN_MEMBERS")) return msg.reply("Sorry! I don't have the permission to ban someone!")
  
-    if(!args[0]) return message.reply("Please mention at least one person or give atleast one id to ban!")
-    if(!args[1]) return message.reply("Please give me a reason to ban this person!")
+    if(!args[0]) return msg.reply("Please mention at least one person or give atleast one id to ban!")
+    if(!args[1]) return msg.reply("Please give me a reason to ban this person!")
  
-    const user = message.guild.members.cache.get(message.mentions.users.first().id || message.guild.members.get(args[0])).id
+    const user = msg.guild.members.cache.get(msg.mentions.users.first().id || msg.guild.members.get(args[0])).id
  
-    if(!user) return message.reply("I can't seem to find this user. Is the id or mention correct?")
+    if(!user) return msg.reply("I can't seem to find this user. Is the id or mention correct?")
  
-    if(user.permissions.has("MANAGE_MESSAGES")) return message.reply("Couldn't kick a moderator.")
+    if(user.permissions.has("MANAGE_msgS")) return msg.reply("Couldn't kick a moderator.")
  
     var reason = args[1].slice(1).join(" ")
  
-    const unbanned = new MessageEmbed()
+    const unbanned = new msgEmbed()
         .setColor("RED")
         .setDescription(`Unbanned **${user} (${user.id})** For **${reason}**`)
         .setFooter(`Requested by: ${msg.author.username}`, msg.author.displayAvatarURL())
@@ -21,11 +21,11 @@ module.exports.run = async(client, msg, args) => {
  
     user.unban().catch(err => {
         if(err) {
-            message.reply('Something went wrong')
+            msg.reply('Something went wrong')
             console.log(err)
         }
     })
-    message.reply({embeds: [unbanned]})
+    msg.reply({embeds: [unbanned]})
 }
 
 module.exports.help = {
