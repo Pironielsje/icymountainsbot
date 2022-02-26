460dfbf05905c8d9407c53b42b79547d90c32cb8
GlowCat <81696606+Pironielsje@users.noreply.github.com>
2022-02-26 16:50:25 +0100

    BotInfo

---
+++ commands/botinfo.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports.run = async(client, msg, args) => {
+
+    const reply = new MessageEmbed()
+        .setTitle(`Botinfo`)
+        .setColor(`AQUA`)
+        .setFields(
+            {name: 'Users', value: client.users.cache.size()},
+            {name: 'Uptime', value: client.uptime}
+        )
+        .setFooter(`Requested by: ${msg.author.username}`, msg.author.displayAvatarURL())
+
+}
+
+module.exports.help = {
+    name: "botinfo",
+    aliases: ["bi"]
+}
