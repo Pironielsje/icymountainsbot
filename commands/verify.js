1a014328c61157ca56dad2f185f5cd3c901054e7
GlowCat <81696606+Pironielsje@users.noreply.github.com>
2022-02-26 09:33:32 +0100

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,7 +1,7 @@
 module.exports.run = async(client, msg, args) => {
     const role = 942069272816017458
 
-    if(msg.member.roles.get(role)) return msg.reply("You already have the role!")
+    if(msg.member.roles.find(role)) return msg.reply("You already have the role!")
 
     else {
         msg.member.roles.add(role)
