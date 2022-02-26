820314a189bc36f5b2339b8574a7b607f3331d4d
GlowCat <81696606+Pironielsje@users.noreply.github.com>
2022-02-26 16:31:31 +0100

    finishing touches verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,7 +1,12 @@
 module.exports.run = async(client, msg, args) => {
 
     if(msg.member.roles.cache.some(role => role.name === "Verified")){
-        return msg.reply("You already have the role!")
+        return msg.reply("You already have the role!").then(mes => {
+            setTimeout(() => {
+                mes.delete()
+                msg.delete()
+            }, 4000);
+        })
     }
     else {
         msg.member.roles.add("942069272816017458")
