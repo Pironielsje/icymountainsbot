49fec04e32a53d54df4b21b32747dfb200e62b83
GlowCat <81696606+Pironielsje@users.noreply.github.com>
2022-02-28 17:32:58 +0100

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,5 +1,5 @@
 module.exports.run = async(client, msg, args) => {
-    if(msg.guild.channels.get(channel => channel.name === `${msg.author.username}-ticket`)) {
+    if(msg.guild.channels.cache.find(channel => channel.name === `${msg.author.username}-ticket`)) {
         return msg.reply(`You already have a ticket smh!`)
     } else {
         msg.guild.channels.create(`${msg.author.username}-ticket`)
