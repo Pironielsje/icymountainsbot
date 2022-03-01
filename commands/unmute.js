823e60a986741b629827cda5f599ce530366e7cc
AlgoGames <81696606+Pironielsje@users.noreply.github.com>
2022-03-01 14:25:42 +0100

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -4,7 +4,7 @@ module.exports.run = async(client, msg, args) => {
 
     if (!msg.member.permissions.has("KICK_MEMBERS")) return msg.reply(`You don't have the permission to mute someone!`)
 
-    const target = msg.guild.members.cache.get(msg.mentions.users.first().id || msg.guild.members.cache.get(args[0]));
+    const target = msg.guild.members.cache.get(msg.mentions.users.first().id) || msg.guild.members.cache.get(args[0]);
     if (!args[0]) return msg.reply(`Provide a member to mute please`)
 
     let mainRole = msg.guild.roles.cache.find(role => role.name === "Verified");
