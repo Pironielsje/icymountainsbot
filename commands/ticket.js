19281d353c30af02bb697528b5cecc03c0975121
AlgoGames <81696606+Pironielsje@users.noreply.github.com>
2022-03-01 10:55:05 +0100

    ticket system!

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,71 +1,56 @@
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js")
 
 module.exports.run = async(client, msg, args) => {
-    const categoryId = "947895136812158956";
+    let author = msg.author.toLowerCase()
+    let discriminator = msg.author.discriminator
 
-    // Get username
-    var userName = msg.author.username;
-    // Verkrijg discriminator
-    var userDiscriminator = msg.author.discriminator;
+    if (msg.guild.channels.cache.find(c => c.name == `${author}${discriminator}-ticket`)) {
+        msg.reply("You already have a ticket open!")
+    } else if (!msg.guild.channels.cache.find(c => c.name == `${author}${discriminator}-ticket`)) {
+        msg.guild.channels.create(`${author}${discriminator}-ticket`, {
+            parent: '947895136812158986',
+            topic: `${author}'s topic!`,
+            permissionOverwrites: [{
+                    id: msg.author.id,
+                    allow: ['SEND_MESSAGES', 'VIEW_CHANNEL']
+                },
+                {
+                    id: client.user.id,
+                    allow: ['SEND_MESSAGES', 'VIEW_CHANNEL']
+                }, {
+                    id: '942822344928415745',
+                    allow: ['SEND_MESSAGES', 'VIEW_CHANNEL']
+                }, {
+                    id: '942069272816017458',
+                    deny: ['VIEW_CHANNEL', 'SEND_MESSAGES']
+                },
+            ],
+            type: 'GUILD_TEXT'
+        }).then(async c => {
+            msg.reply(`Ticket created in <#${c.id}>`)
 
-    // If ticket has been made
-    var bool = false;
+            const newtic = new MessageEmbed()
+                .setColor(`NAVY`)
+                .setDescription(`Please wait untill the staff replies! Don't ping them please!`)
 
-    // Checking if ticket has been made.
-    msg.guild.channels.forEach((channel) => {
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setCustomId('close')
+                    .setLabel('Close')
+                    .setEmoji("🔐")
+                    .setStyle("DANGER")
+                )
 
-        // If ticket has been made sent:
-        if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
-
-            msg.channel.send("You already made a ticket");
-
-            bool = true;
-
-        }
-
-    });
-
-    // Ticket return code
-    if (bool == true) return;
-
-    var embedCreateTicket = new discord.RichEmbed()
-        .setTitle("Hey, " + msg.author.username)
-        .setFooter("Support channel will be made");
-
-    msg.channel.send(embedCreateTicket);
-
-    // Create channel and put it in the right catogary
-    msg.guild.createChannel(userName + "-" + userDiscriminator, "text").then((createdChan) => { // Maak kanaal
-
-        createdChan.setParent(categoryId).then((settedParent) => { // Zet kanaal in category.
-
-            // Put permissions for everyone
-            settedParent.overwritePermissions(msg.guild.roles.find('name', "@everyone"), { "READ_MESSAGES": false });
-            settedParent.overwritePermissions(msg.guild.roles.find('name', "@staff"), { "VIEW_CHANNEL": true });
-            // Put permission by the user that created the ticket
-            settedParent.overwritePermissions(msg.author, {
-
-                "READ_MESSAGES": true,
-                "SEND_MESSAGES": true,
-                "ATTACH_FILES": true,
-                "CONNECT": true,
-                "CREATE_INSTANT_INVITE": false,
-                "ADD_REACTIONS": true
-
-            });
-
-            var embedParent = new discord.RichEmbed()
-                .setTitle("Hey, " + msg.author.username.toString())
-                .setDescription("Ask your questions to the staff team here!");
-
-            settedParent.send(embedParent);
-        }).catch(err => {
-            msg.channel.send("Something went wrong.");
-        });
-
-    }).catch(err => {
-        msg.channel.send("Something went wrong.");
-    });
+            c.send({
+                content: `<@${msg.author.id}> <@942822344928415745>`,
+                embeds: [newtic],
+                components: [row]
+            }).then(m => {
+                m.pin()
+            })
+        })
+    }
 }
 
 module.exports.help = {
--- index.js
+++ index.js
@@ -1,71 +1,81 @@
-const { Client, Intents, Collection } = require("discord.js");
+const { Client, Intents, Collection, MessageEmbed } = require("discord.js");
 const config = require("./config.json");
 const mongoose = require("mongoose");
 const fs = require("fs");
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 client.commands = new Collection();
 client.aliases = new Collection();
 
 const cmdFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
 
 for (const file of cmdFiles) {
-  const command = require(`./commands/${file}`);
+    const command = require(`./commands/${file}`);
 
-  console.log(`${file} loaded`);
+    console.log(`${file} loaded`);
 
-  client.commands.set(command.help.name, command);
+    client.commands.set(command.help.name, command);
 
-  for (const alias of command.help.aliases) {
-    client.aliases.set(alias, command);
-  }
+    for (const alias of command.help.aliases) {
+        client.aliases.set(alias, command);
+    }
 }
 
 mongoose
-  .connect(process.env.MONGOSRV, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to the db");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+    .connect(process.env.MONGOSRV, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to the db");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 client.once("ready", () => {
-  console.log(`${client.user.username} is ready!`);
-  client.user.setActivity(`;help`, { type: "PLAYING" });
+    console.log(`${client.user.username} is ready!`);
+    client.user.setActivity(`;help`, { type: "PLAYING" });
 });
 
-client.on("messageCreate", async (msg) => {
-  if (msg.author.bot) return;
+client.on("messageCreate", async(msg) => {
+    if (msg.author.bot) return;
 
-  const msgArray = msg.content.split(" ");
+    const msgArray = msg.content.split(" ");
 
-  const command = msgArray[0];
+    const command = msgArray[0];
 
-  const args = msgArray.slice(1);
-  
-  if (!msg.content.startsWith(config.prefix)) return;
-  
-  const cmdData =
-    client.commands.get(command.slice(config.prefix.length)) ||
-    client.aliases.get(command.slice(config.prefix.length));
-  
-  if (!cmdData) return;
+    const args = msgArray.slice(1);
 
-  try {
-    cmdData.run(client, msg, args);
-  } catch (error) {
-    console.log(error);
-    msg.reply(`Something went wrong please contact Niels#8069`);
-  }
+    if (!msg.content.startsWith(config.prefix)) return;
+
+    const cmdData =
+        client.commands.get(command.slice(config.prefix.length)) ||
+        client.aliases.get(command.slice(config.prefix.length));
+
+    if (!cmdData) return;
+
+    try {
+        cmdData.run(client, msg, args);
+    } catch (error) {
+        console.log(error);
+        msg.reply(`Something went wrong please contact Niels#8069`);
+    }
 });
+
+client.on('interaction', (interaction) => {
+    if (interaction.customId === "close") {
+        interaction.channel.setName(`closed-${interaction.user.username.toLowerCase()}${interaction.user.discriminator}-ticket`)
+        const reply = new MessageEmbed()
+            .setTitle("Closed")
+            .setColor("RED")
+            .setDescription(`Deleting the ticket in 5 seconds...\nClosed by ${interaction.user.username}`)
+    }
+})
 
 client.login(process.env.TOKEN);
